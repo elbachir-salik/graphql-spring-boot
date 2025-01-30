2969037a7b7ddac567b9924493112d6cab3738a2
elbachir-salik <salik.elbachir@gmail.com>
2025-01-30 11:45:22 +0100

    added a query to have a book by id

--- src/main/java/org/app/graphql/controllers/BookController.java
+++ src/main/java/org/app/graphql/controllers/BookController.java
@@ -2,10 +2,12 @@ package org.app.graphql.controllers;
 
 
 import org.app.graphql.models.Book;
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class BookController {
@@ -13,6 +15,11 @@ public class BookController {
     @QueryMapping
     public List<Book> books() {
         return Book.books;
+    }
+
+    @QueryMapping
+    public Optional<Book> bookById(@Argument Integer id) {
+        return Book.getBookById(id);
     }
 }
 
--- src/main/java/org/app/graphql/models/Book.java
+++ src/main/java/org/app/graphql/models/Book.java
@@ -3,6 +3,7 @@ package org.app.graphql.models;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public record Book(Integer id,
                    String name,
@@ -11,4 +12,10 @@ public record Book(Integer id,
             new Book(1, "Harry Potter and the Philosopher's Stone J. K. Rowling",580),
             new Book(2, "Harry Potter and the Chamber of Secrets J. K. Rowling",420)
     );
+
+    public static Optional<Book> getBookById(Integer id) {
+        return books.stream()
+                .filter(b ->b.id.equals(id))
+                .findFirst();
+    }
 }
