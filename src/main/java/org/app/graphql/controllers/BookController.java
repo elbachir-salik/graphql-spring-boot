f5850b357a4f47fc443a8d119211e76d18cf68a4
elbachir-salik <salik.elbachir@gmail.com>
2025-01-30 12:23:28 +0100

    mapped fields: {Book=[author]}

--- src/main/java/org/app/graphql/controllers/BookController.java
+++ src/main/java/org/app/graphql/controllers/BookController.java
@@ -1,9 +1,11 @@
 package org.app.graphql.controllers;
 
 
+import org.app.graphql.models.Author;
 import org.app.graphql.models.Book;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
@@ -20,6 +22,11 @@ public class BookController {
     @QueryMapping
     public Optional<Book> bookById(@Argument Integer id) {
         return Book.getBookById(id);
+    }
+
+    @SchemaMapping
+    public Optional<Author> author(Book book) {
+        return Author.getAuthorById(book.authorId());
     }
 }
 
--- src/main/java/org/app/graphql/models/Book.java
+++ src/main/java/org/app/graphql/models/Book.java
@@ -7,11 +7,12 @@ import java.util.Optional;
 
 public record Book(Integer id,
                    String name,
-                   Integer pageCount) {
+                   Integer pageCount,
+                   Integer authorId) {
     public static List<Book> books = Arrays.asList(
-            new Book(1, "Harry Potter and the Philosopher's Stone J. K. Rowling",580),
-            new Book(2, "Harry Potter and the Chamber of Secrets J. K. Rowling",420),
-            new Book(3, "the art of war",655)
+            new Book(1, "Harry Potter and the Philosopher's Stone J. K. Rowling",580,1),
+            new Book(2, "Harry Potter and the Chamber of Secrets J. K. Rowling",420,1),
+            new Book(3, "the art of war",655,2)
     );
 
     public static Optional<Book> getBookById(Integer id) {
